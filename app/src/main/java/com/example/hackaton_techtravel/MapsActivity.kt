0a524c112c1ebd5323c82f88d3ccc74c4b333bae
czar <cesarandresolarte@gmail.com>
2024-04-06 23:50:46 -0500

    Added functionality for camera and location permissions

--- app/src/main/java/com/example/hackaton_techtravel/MapsActivity.kt
+++ app/src/main/java/com/example/hackaton_techtravel/MapsActivity.kt
@@ -1,7 +1,17 @@
 package com.example.hackaton_techtravel
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.hackaton_techtravel.PermissionCodes.Companion.CAMERA_PERMISSION_CODE
+import com.example.hackaton_techtravel.PermissionCodes.Companion.LOCATION_PERMISSION_CODE
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,10 +20,13 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.hackaton_techtravel.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private var mMap: GoogleMap? = null
+    private var geocoder: Geocoder? = null
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
     private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +34,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setUpButtonsWithoutPermissions()
+        setUpEventListenersWithoutPermissions()
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -40,9 +56,193 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        mMap!!.uiSettings.isZoomGesturesEnabled = true
+
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun setUpButtonsWithoutPermissions() {
+        binding.mapXButton.setOnClickListener {
+            binding.selectedLocation.text = ""
+        }
+
+        binding.mapSearchButton.setOnClickListener {
+            searchLocationWithGeoCoder(binding.searchLocationMap.text.toString())
+        }
+
+        binding.mapLocationButton.setOnClickListener {
+            checkPermissionForLocation(this)
+        }
+
+        binding.mapAddImageButton.setOnClickListener {
+            checkPermissionForCamera(this)
+        }
+    }
+
+    private fun setUpEventListenersWithoutPermissions() {
+
+    }
+
+    private fun setUpButtonsWithLocationPermission() {
+        binding.mapLocationButton.setOnClickListener {
+            val latIng = getCurrentLocation()
+            setUpMarker(latIng)
+        }
+    }
+
+    private fun disableButtonsWithLocationPermission() {
+        // Deactivate button
+        binding.mapLocationButton.isEnabled = false
+    }
+
+    private fun setUpButtonsWithCameraPermission() {
+        binding.mapAddImageButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+    }
+
+    private fun disableButtonsWithCameraPermission() {
+        // Deactivate button
+        binding.mapAddImageButton.isEnabled = false
+    }
+
+    private fun searchLocationWithGeoCoder(location: String) {
+        if(location.isEmpty() || geocoder == null) {
+            Toast.makeText(this, "Location is empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val addressList = geocoder!!.getFromLocationName(location, 1)
+
+        if (!addressList.isNullOrEmpty()) {
+            val address = addressList[0]
+            val latLng = LatLng(address.latitude, address.longitude)
+
+            if (mMap == null) {
+                return
+            }
+
+            setUpMarker(latLng)
+        } else {
+            Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation(): LatLng {
+        var latLng = LatLng(0.0, 0.0)
+        mFusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                latLng = LatLng(location.latitude, location.longitude)
+            } else {
+                Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return latLng
+    }
+
+    private fun setUpMarker(latLng: LatLng) {
+        if (mMap == null) {
+            return
+        }
+        mMap!!.clear()
+        mMap!!.addMarker(MarkerOptions().position(latLng).title("Avistamiento"))
+        mMap!!.moveCamera(CameraUpdateFactory.zoomTo(30f))
+        mMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+    }
+
+    private fun checkPermissionForLocation(activity: AppCompatActivity) {
+        when{
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                // Permission already granted
+                setUpButtonsWithLocationPermission()
+            }
+
+            shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                // If user previously denied the permission
+                Toast.makeText(this, "Permission previously denied", Toast.LENGTH_SHORT).show()
+                 requestPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_PERMISSION_CODE, "Needed for locating person")
+            }
+
+            else -> {
+                // Always call the own function to request permission, not the system one (requestPermissions)
+                requestPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_PERMISSION_CODE, "Needed for locating person")
+            }
+
+        }
+    }
+
+    private fun checkPermissionForCamera(activity: AppCompatActivity) {
+        when{
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+                // Permission already granted
+                setUpButtonsWithCameraPermission()
+            }
+
+            shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA) -> {
+                // If user previously denied the permission
+                Toast.makeText(this, "Permission previously denied", Toast.LENGTH_SHORT).show()
+                requestPermission(this, android.Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE, "Needed for taking pictures")
+            }
+
+            else -> {
+                // Always call the own function to request permission, not the system one (requestPermissions)
+                requestPermission(this, android.Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE, "Needed for taking pictures")
+            }
+
+        }
+    }
+
+    private fun requestPermission(context: Activity, permission: String, requestCode: Int, justify: String) {
+        if(ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+            if(shouldShowRequestPermissionRationale(permission)) {
+                Toast.makeText(this, justify, Toast.LENGTH_SHORT).show()
+            }
+            requestPermissions(arrayOf(permission), requestCode)
+        } else {
+            when (requestCode) {
+                LOCATION_PERMISSION_CODE -> {
+                    setUpButtonsWithLocationPermission()
+                }
+                CAMERA_PERMISSION_CODE -> {
+                    setUpButtonsWithCameraPermission()
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            LOCATION_PERMISSION_CODE -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission granted
+                    setUpButtonsWithLocationPermission()
+                } else {
+                    // Permission denied
+                    disableButtonsWithLocationPermission()
+                }
+                return
+            }
+
+            CAMERA_PERMISSION_CODE -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission granted
+                    setUpButtonsWithCameraPermission()
+                } else {
+                    // Permission denied
+                    disableButtonsWithCameraPermission()
+                }
+                return
+            }
+
+            else -> {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/hackaton_techtravel/PermissionCodes.kt
@@ -0,0 +1,8 @@
+package com.example.hackaton_techtravel
+
+class PermissionCodes {
+    companion object {
+        const val LOCATION_PERMISSION_CODE = 101
+        const val CAMERA_PERMISSION_CODE = 102
+    }
+}
